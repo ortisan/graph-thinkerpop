5e10ad1ef970404b2812c28bf4b16bcd716b4e70
Marcelo Ortiz de Santana <tentativafc@gmail.com>
2022-10-01 23:20:00 -0300

    Instructions to connect into gremlin-server from console

--- README.md
+++ README.md
@@ -7,6 +7,32 @@
 docker-compose up --build
 # Configuring project
 poetry init
+# Run formatter and lint
+poetry run autoflake --expand-star-imports --remove-all-unused-imports --remove-duplicate-keys --remove-unused-variables --recursive --in-place . && poetry run isort . && poetry run black .
 # Creating graph
 python main.py
 ```
+
+## Gremlin Console 
+Configuring gremlin-console to reach docker container of gremlin-server:
+
+- Clone tinkerpop/gremlin-console and create custom remote file point to docker dns internal:
+
+    ```sh
+    echo "hosts: [host.docker.internal]
+    port: 8182
+    serializer: { className: org.apache.tinkerpop.gremlin.driver.ser.GraphBinaryMessageSerializerV1, config: { serializeResultToString: true }}" > conf/remote-docker.yaml
+    ```
+- Rebuild the tinkerpop/gremlin-console:
+    ```sh
+    mvn clean install
+    docker build --build-arg GREMLIN_CONSOLE_DIR=. -t tentativafc/gremlin-console .
+    docker push tentativafc/gremlin-console
+    ```
+- Start gremlin-console:
+    ```sh
+    docker run-ti tentativafc/gremlin-console
+    # Connects to remote server
+    :remote connect tinkerpop.server conf/remote-docker.yaml
+    :remote console
+    ```
--- main.py
+++ main.py
@@ -1,27 +1,46 @@
-from datetime import datetime
 import uuid
-from gremlin_python.process.anonymous_traversal import traversal
-from gremlin_python import statics
-from gremlin_python.structure.graph import Graph
-from gremlin_python.process.graph_traversal import __
-from gremlin_python.process.strategies import *
+from datetime import datetime
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+from gremlin_python.process.anonymous_traversal import traversal
+
 
 def get_graph():
-  g = traversal().withRemote(DriverRemoteConnection('ws://localhost:8182/gremlin'))
-  return g
+    g = traversal().withRemote(DriverRemoteConnection("ws://localhost:8182/gremlin"))
+    return g
+
 
 if __name__ == "__main__":
-  g = get_graph()
-  g.V().drop().iterate()
+    g = get_graph()
+    g.V().drop().iterate()
 
-  id = str(uuid.uuid4())
-  
-  v1 = g.addV('states').property("type", "state").property("state","starting_paid").property("receive_date",datetime.now().isoformat()).property("id_payment", id).next()
-  v2 = g.addV("states").property("type", "state").property("state", "waiting_authorization").property("receive_date",datetime.now().isoformat()).property("id_payment", id).next()
-  v3 = g.addV("states").property("type", "state").property("state", "paid").property("receive_date",datetime.now().isoformat()).property("id_payment", id).next()
+    id = str(uuid.uuid4())
 
-  g.V(v1).addE("changed").to(v2).iterate()
-  g.V(v2).addE("changed").to(v3).iterate()
-  
-  
+    v1 = (
+        g.addV("states")
+        .property("type", "state")
+        .property("state", "starting_paid")
+        .property("received_date", datetime.timestamp(datetime.now()))
+        .property("id_payment", id)
+        .next()
+    )
+
+    v2 = (
+        g.addV("states")
+        .property("type", "state")
+        .property("state", "waiting_authorization")
+        .property("received_date", datetime.timestamp(datetime.now()))
+        .property("id_payment", id)
+        .next()
+    )
+
+    v3 = (
+        g.addV("states")
+        .property("type", "state")
+        .property("state", "paid")
+        .property("received_date", datetime.timestamp(datetime.now()))
+        .property("id_payment", id)
+        .next()
+    )
+
+    g.V(v1).addE("changed").to(v2).iterate()
+    g.V(v2).addE("changed").to(v3).iterate()
